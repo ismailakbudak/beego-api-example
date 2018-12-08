bd15745b1ae1b4d832c47fc8943be3f28f797e7a
İsmail Akbudak <ismail.akbudak@lab2023.com>
2018-12-08 20:33:26 +0300

    Add Procfile for heroku

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
+	"os"
 	_ "reporting-api/routers"
+	"strconv"
 
 	"github.com/astaxie/beego"
 )
@@ -11,5 +14,21 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	log.Println("Env $PORT :", os.Getenv("PORT"))
+	if os.Getenv("PORT") != "" {
+		port, err := strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			log.Fatal(err)
+			log.Fatal("$PORT must be set")
+		}
+		log.Println("port : ", port)
+		beego.BConfig.Listen.HTTPPort = port
+		beego.BConfig.Listen.HTTPSPort = port
+	}
+	if os.Getenv("BEEGO_ENV") != "" {
+		beego.BConfig.RunMode = os.Getenv("BEEGO_ENV")
+	}
+
 	beego.Run()
 }
